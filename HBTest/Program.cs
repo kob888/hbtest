6bd5a9633540cc0cd622b5aa544449c8ed65547a
Vladislav Grachev <vladgrachev88@gmail.com>
2019-03-22 10:49:36 +0300

    Add project files.

---
+++ HBTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HBTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
